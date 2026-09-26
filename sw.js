9c71142d6e4a73ed01f0f60a979ab64b7ecd7c6f
hussein1labtop-jpg <hussein1labtop@gmail.com>
2026-09-26 03:26:50 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('activate', e => self.clients.claim());
-self.addEventListener('fetch', e => {
-  // بدون تخزين مؤقت — بس موجود حتى المتصفح يعتبر الموقع "تطبيق" قابل للتثبيت
-});
